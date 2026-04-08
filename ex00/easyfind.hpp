77403b4a194d8e1175f0cf03a4ed26eea85c6acb
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-04-08 14:10:13 +0200

    Use std::exception instead of custom exception
    Replaced custom ValueNotFoundException with std::exception.

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -17,24 +17,16 @@
 # include <exception>
 # include <iterator>
 
-class ValueNotFoundException : public std::exception
-{
-public:
-	virtual const char* what() const throw()
-	{
-		return "Value not found in container";
-	}
-};
-
 template <typename T>
 typename T::iterator easyfind(T & container, int value)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
 	{
-		throw ValueNotFoundException();
+		throw std::exception();
 	}
 	return it;
 }
 
 #endif
+
